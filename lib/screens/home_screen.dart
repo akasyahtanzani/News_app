1775d7af9dfdfb08722a2ae8e17da0430539318b
akasyahtanzani <akasyahtanzani@gmail.com>
2025-10-22 10:54:58 +0700

    finishing home screen ui with bug

---
+++ lib/screens/home_screen.dart
@@ -0,0 +1,193 @@
+// A brand new for make a screen using get state management
+
+import 'package:flutter/foundation.dart';
+import 'package:flutter/material.dart';
+import 'package:get/get.dart';
+import 'package:news/controllers/news_controller.dart';
+import 'package:news/utils/app_colors.dart';
+import 'package:news/widgets/category_chip.dart';
+import 'package:news/widgets/loading_shimmer.dart';
+import 'package:news/widgets/news_card.dart';
+
+class HomeScreen extends GetView<NewsController> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('News App'),
+        centerTitle: true,
+        actions: [
+          IconButton(
+            icon: Icon(Icons.search),
+            onPressed: () => _showSearchDialog(context),
+          )
+        ],
+      ),
+      body: Column(
+        children: [
+          // categories
+          Container(
+            height: 60,
+            color: Colors.white,
+            child: ListView.builder(
+              scrollDirection: Axis.horizontal,
+              padding: EdgeInsets.symmetric(horizontal: 16, vertical: 8),
+              itemCount: controller.Categories.length,
+              itemBuilder: (context, index) {
+                final category = controller.Categories[index];
+                // Obx = observable dari get x
+              return Obx(() => CategoryChip(
+                label: category.capitalize ?? category,
+                isSelected: controller.selectedCategory == category,
+                onTap: () => controller.selectCategory(category),
+              ));
+              },
+            ),
+          ),
+
+          //news list 
+          Expanded(
+            child:Obx(() {
+              if (controller.isLoading) {
+                return LoadingShimmer();
+              }
+              if (controller.Error.isNotEmpty) {
+                return _buildErrorWidget();
+              }
+
+              if(controller.articles.isEmpty) {
+                return _buildEmptyWidget();
+              }
+
+              return RefreshIndicator(
+                onRefresh: controller.refreshNews,
+                child: ListView.builder(
+                  padding: EdgeInsets.all(16),
+                  itemCount: controller.articles.length,
+                  itemBuilder: (context, index) {
+                    final article = controller.articles[index];
+                    return NewsCard(
+                      article: article,
+                      onTap: () => Get.toNamed(
+                      //TODO: add route to detail screen
+                      
+                      //argument berfungsi untuk bernavigasi ke halaman lain dengan membawa data 
+                      arguments: article
+                      ),
+                    );
+                  },
+                ),
+              );
+            }) ,
+          )
+        ],
+      ),
+    );
+  }
+
+  Widget _buildEmptyWidget() {
+    return Center(
+      child: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+          Icon(
+             Icons.newspaper,
+            size: 64,
+            color: AppColors.textHint,
+          ),
+          SizedBox(height: 16),
+          Text(
+            'No news available',
+            style: TextStyle(
+              fontSize: 18,
+              fontWeight: FontWeight.bold,
+              color: AppColors.textPrimary
+            ),
+          ),
+          SizedBox(height: 8),
+          Text(
+            'please try again later',
+            style: TextStyle(
+              color: AppColors.textSecondary
+            ),
+          )
+        ],
+      ),
+    );
+  }
+
+  Widget _buildErrorWidget() {
+    return Center(
+      child: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+          Icon(
+            Icons.error_outline,
+            size: 64,
+            color: AppColors.error,
+          ),
+          SizedBox(height: 16),
+          Text(
+            'Something Went Wrong',
+          style: TextStyle(
+          fontSize: 18,
+          fontWeight: FontWeight.bold,
+          color: AppColors.textPrimary
+        ),  
+          ),
+          SizedBox(height: 8),
+          Text(
+            'please chek your internet connection',
+            style: TextStyle(
+              color: AppColors.textSecondary
+            ),
+          ),
+          SizedBox(height: 24),
+          ElevatedButton(
+            onPressed: controller.refreshNews,
+            child: Text('Retry'),
+          )
+        ],
+      ),
+    );
+  }
+
+  
+  void _showSearchDialog(BuildContext context) {
+    final TextEditingController searchController = TextEditingController();
+    showDialog(
+      context: context,
+      builder: (context) => AlertDialog(
+        title: Text('search news'),
+        content: TextField(
+          controller: searchController,
+          decoration: InputDecoration(
+            hintText: 'please type a news...',
+            border: OutlineInputBorder()
+          ), 
+          onSubmitted: (value) {
+            if (value.isNotEmpty) {
+              controller.searchNews(value);
+              Navigator.of(context).pop();
+            }
+          },
+        ),
+        actions: [
+          TextButton(
+            onPressed: () => Navigator.of(context).pop(),
+            child: Text('cancel'),
+          ),
+          ElevatedButton(
+            onPressed: () {
+              if (searchController.text.isNotEmpty) {
+                controller.searchNews(searchController.text);
+                Navigator.of(context).pop();
+              }
+            },
+            child: Text('search'),
+          )
+        ],
+      ),
+    );
+  }
+}
